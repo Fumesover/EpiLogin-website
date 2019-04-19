5078c0f3a40e9cacbebd7ae8aeb46df5141d2c79
Albin Parou <fumesover@gmail.com>
2019-04-19 11:49:46 +0900

    Feat (api): Add authentication for api

--- config/settings.py
+++ config/settings.py
@@ -111,9 +111,9 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
     ),
-    'DEFAULT_PERMISSION_CLASSES': (
-        # 'rest_framework.permissions.IsAuthenticated',
-        # 'rest_framework.permissions.DjangoModelPermissions',
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 100,
--- website/apps/api/views.py
+++ website/apps/api/views.py
@@ -4,6 +4,8 @@ from rest_framework import viewsets
 from django_filters import rest_framework as filters
 from rest_framework.decorators import action
 from django.shortcuts import get_object_or_404
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication
 
 from .serializers import *
 from website.apps.groups.models import Group, Ban, Update
@@ -11,26 +13,41 @@ from website.apps.members.models import Member
 from website.apps.servers.models import Server, Rank
 
 class UpdateViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = (TokenAuthentication, SessionAuthentication)
+
     queryset = Update.objects.all()
     serializer_class = UpdateSerializer
     filter_backends = (filters.DjangoFilterBackend,)
     filterset_fields = ('type', 'ban_type', 'server', 'email')
 
 class GroupViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = (TokenAuthentication, SessionAuthentication)
+
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
     filter_backends = (filters.DjangoFilterBackend,)
     filterset_fields = ('email', 'group')
 
 class RankViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = (TokenAuthentication, SessionAuthentication)
+
     queryset = Rank.objects.all()
     serializer_class = RankSerializer
 
 class ServerViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = (TokenAuthentication, SessionAuthentication)
+
     queryset = Server.objects.all()
     serializer_class = ServerSerializer
 
 class MemberViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = (TokenAuthentication, SessionAuthentication)
+
     queryset = Member.objects.all()
     serializer_class = MemberSerializer
     filter_backends = (filters.DjangoFilterBackend,)
@@ -49,7 +66,7 @@ class MemberViewSet(viewsets.ModelViewSet):
 #        else:
 #            return Response({'status': 'Invalid request'}, status=400)
 
-    @action(detail=True, methods=['post', 'delete'])
+    @action(detail=True, methods=['post', 'delete'], permission_classes=(IsAuthenticated,))
     def server(self, request, pk=None):
         member = get_object_or_404(Member, pk=pk)
 
